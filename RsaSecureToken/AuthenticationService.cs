f5a1aa093a8e9b280ba6b23c87cab15fa683dfe0
hatelove <jay.hatelove@gmail.com>
2018-03-30 15:00:34 +0800

    mock

--- RsaSecureToken/AuthenticationService.cs
+++ RsaSecureToken/AuthenticationService.cs
@@ -5,20 +5,38 @@ namespace RsaSecureToken
 {
     public class AuthenticationService
     {
+        private readonly IProfile _profile;
+        private readonly IToken _token;
+        private readonly ILog _log;
+
+        public AuthenticationService()
+        {
+            _profile = new ProfileDao();
+            _token = new RsaTokenDao();
+        }
+
+        public AuthenticationService(IProfile profile, IToken token, ILog log)
+        {
+            _profile = profile;
+            _token = token;
+            _log = log;
+        }
+
         public bool IsValid(string account, string password)
         {
             // 根據 account 取得自訂密碼
-            var profileDao = new ProfileDao();
-            var passwordFromDao = profileDao.GetPassword(account);
+            //IProfile profileDao = new ProfileDao();
+            var passwordFromDao = _profile.GetPassword(account);
 
             // 根據 account 取得 RSA token 目前的亂數
-            var rsaToken = new RsaTokenDao();
-            var randomCode = rsaToken.GetRandom(account);
+            //IToken rsaToken = new RsaTokenDao();
+            var randomCode = _token.GetRandom(account);
 
             // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
             var validPassword = passwordFromDao + randomCode;
             var isValid = password == validPassword;
 
+            _log.Save($"{account}  login failed.");
             if (isValid)
             {
                 return true;
@@ -30,7 +48,22 @@ namespace RsaSecureToken
         }
     }
 
-    public class ProfileDao
+    public interface ILog
+    {
+        void Save(string message);
+    }
+
+    public interface IProfile
+    {
+        string GetPassword(string account);
+    }
+
+    public interface IToken
+    {
+        string GetRandom(string account);
+    }
+
+    public class ProfileDao : IProfile
     {
         public string GetPassword(string account)
         {
@@ -55,7 +88,7 @@ namespace RsaSecureToken
         }
     }
 
-    public class RsaTokenDao
+    public class RsaTokenDao : IToken
     {
         public string GetRandom(string account)
         {
--- RsaSecureTokenTests/AuthenticationServiceTests.cs
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using RsaSecureToken;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
 namespace RsaSecureToken.Tests
 {
     [TestClass()]
@@ -13,12 +15,39 @@ namespace RsaSecureToken.Tests
         [TestMethod()]
         public void IsValidTest()
         {
-            var target = new AuthenticationService();
+            var profile = Substitute.For<IProfile>();
+            profile.GetPassword("").ReturnsForAnyArgs("91");
+
+            var token = Substitute.For<IToken>();
+            token.GetRandom("").ReturnsForAnyArgs("000000");
+
+            var log = Substitute.For<ILog>();
+
+            var target = new AuthenticationService(profile, token, log);
 
             var actual = target.IsValid("joey", "91000000");
 
             //always failed
-            Assert.IsTrue(actual);                       
+            Assert.IsTrue(actual);
+        }
+
+
+        [TestMethod()]
+        public void Invalid_should_log()
+        {
+            var profile = Substitute.For<IProfile>();
+            profile.GetPassword("").ReturnsForAnyArgs("91");
+
+            var token = Substitute.For<IToken>();
+            token.GetRandom("").ReturnsForAnyArgs("000000");
+
+            var log = Substitute.For<ILog>();
+
+            var target = new AuthenticationService(profile, token, log);
+
+            var actual = target.IsValid("joey", "wrong password");
+            log.Received(1).Save(Arg.Is<string>(s => s.Contains("joey")));
+            //log.Received(1).Save("joey login failed");
         }
     }
 }
