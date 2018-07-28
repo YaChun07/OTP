6a0bee6d9fc72af5a6e9c28247730405bfb1e701
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-07-28 13:57:44 +0800

    implement OPT
    1. 把強耦合的物件抽成介面
    2. 抽到field
    3. 由外部決定物件->建立建構子，空的建構子走原來的路線
    4. 測試端注入

--- RsaSecureToken/AuthenticationService.cs
+++ RsaSecureToken/AuthenticationService.cs
@@ -5,15 +5,27 @@ namespace RsaSecureToken
 {
     public class AuthenticationService
     {
+        private IProfile _profile;
+        private IToken _token;
+
+        public AuthenticationService()
+        {
+            _profile = new Profile();
+            _token = new Token();
+        }
+        public AuthenticationService(IProfile profile, IToken token)
+        {
+            _profile = profile;
+            _token = token;
+        }
+
         public bool IsValid(string account, string password)
         {
             // 根據 account 取得自訂密碼
-            var profileDao = new ProfileDao();
-            var passwordFromDao = profileDao.GetPassword(account);
+            var passwordFromDao = _profile.GetPassword(account);
 
             // 根據 account 取得 RSA token 目前的亂數
-            var rsaToken = new RsaTokenDao();
-            var randomCode = rsaToken.GetRandom(account);
+            var randomCode = _token.GetRandom(account);
 
             // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
             var validPassword = passwordFromDao + randomCode;
@@ -30,7 +42,12 @@ namespace RsaSecureToken
         }
     }
 
-    public class ProfileDao
+    public interface IProfile
+    {
+        string GetPassword(string account);
+    }
+
+    public class Profile : IProfile
     {
         public string GetPassword(string account)
         {
@@ -55,7 +72,12 @@ namespace RsaSecureToken
         }
     }
 
-    public class RsaTokenDao
+    public interface IToken
+    {
+        string GetRandom(string account);
+    }
+
+    public class Token : IToken
     {
         public string GetRandom(string account)
         {
